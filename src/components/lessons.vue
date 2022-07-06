ce7846347acf9b34396aa6414617000d958574d3
Ivan Shavliuga (Ivanov) <38425718+IvanShavliuga@users.noreply.github.com>
2022-07-06 18:28:07 +0300

    new components

--- src/components/lessons.vue
+++ src/components/lessons.vue
@@ -1,47 +1,47 @@
 <template>
-    <section class="lessons">
-      <div class="wrapper">
-        <h2>Как проходят занятия</h2>
-        <div class="item">
-          <div class="item__info">
-            <h3 class="item__info__header">Дыхательные техники</h3>
-            <p class="item__info__desc">
-              Специальные дыхательные упраэнения помогут сконцентрировать
-              внимание и сфокусировать сохнание на мышцах тела
-            </p>
-          </div>
-          <div class="item__image"><div class="item__image__photo1"></div></div>
+  <section class="lessons">
+    <div class="wrapper">
+      <h2>Как проходят занятия</h2>
+      <div class="item">
+        <div class="item__info">
+          <h3 class="item__info__header">Дыхательные техники</h3>
+          <p class="item__info__desc">
+            Специальные дыхательные упраэнения помогут сконцентрировать внимание
+            и сфокусировать сохнание на мышцах тела
+          </p>
         </div>
-        <div class="item">
-          <div class="item__info">
-            <h3 class="item__info__header">Разминка</h3>
-            <p class="item__info__desc">
-              Перед началом занятий разогрев тела, прорабатываем мышцы шеи,
-              спины, выполняем упражнения на баланс под спокойную музыку
-            </p>
-          </div>
-          <div class="item__image"><div class="item__image__photo2"></div></div>
-        </div>
-        <div class="item">
-          <div class="item__info">
-            <h3 class="item__info__header">Основной блок</h3>
-            <p class="item__info__desc">
-              Упражнения выполняются на коврике без специального оборудования
-              или с малым оборудованиемв медленном темпе
-            </p>
-          </div>
-          <div class="item__image"><div class="item__image__photo3"></div></div>
-        </div>
-        <div class="item">
-          <div class="item__info">
-            <h3 class="item__info__header">Стрейчинг</h3>
-            <p class="item__info__desc">
-              В конце выполняем специальные упражнения для развития гибкости,
-              снятия усталости, стресса и мышечного напряжения
-            </p>
-          </div>
-          <div class="item__image"><div class="item__image__photo4"></div></div>
-        </div>
+        <div class="item__image"><div class="item__image__photo1"></div></div>
       </div>
-    </section>
+      <div class="item">
+        <div class="item__info">
+          <h3 class="item__info__header">Разминка</h3>
+          <p class="item__info__desc">
+            Перед началом занятий разогрев тела, прорабатываем мышцы шеи, спины,
+            выполняем упражнения на баланс под спокойную музыку
+          </p>
+        </div>
+        <div class="item__image"><div class="item__image__photo2"></div></div>
+      </div>
+      <div class="item">
+        <div class="item__info">
+          <h3 class="item__info__header">Основной блок</h3>
+          <p class="item__info__desc">
+            Упражнения выполняются на коврике без специального оборудования или
+            с малым оборудованиемв медленном темпе
+          </p>
+        </div>
+        <div class="item__image"><div class="item__image__photo3"></div></div>
+      </div>
+      <div class="item">
+        <div class="item__info">
+          <h3 class="item__info__header">Стрейчинг</h3>
+          <p class="item__info__desc">
+            В конце выполняем специальные упражнения для развития гибкости,
+            снятия усталости, стресса и мышечного напряжения
+          </p>
+        </div>
+        <div class="item__image"><div class="item__image__photo4"></div></div>
+      </div>
+    </div>
+  </section>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,26 +2,11 @@
   <div class="container">
     <appHeader />
     <appAbout />
-    <appOrders/>
-    <appLessons/>
-    <appComments/>
-    <section class="map">
-      <h2>Контакты</h2>
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3387.1816329715443!2d116.165484!3d-31.901661999999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2a32c445d61f42d9%3A0x19f05198e82d3d32!2sLittle+Caesars+Pizzeria!5e0!3m2!1sen!2sin!4v1430720025747"
-        frameborder="0"
-        style="border: 0; width: 100%; height: 750px; margin: 0; padding: 0"
-      ></iframe>
-      <div class="map__contacts">
-        <h3 class="map__header">Адрес</h3>
-        <p class="map__desc">г Москва ул. Молодежная 22а</p>
-        <h3 class="map__header">Телефон:</h3>
-        <p class="map__desc">+7 495 555 55 55</p>
-        <h3 class="map__header">Email</h3>
-        <p class="map__header">pilates@test.ru</p>
-      </div>
-    </section>
+    <appOrders />
+    <appLessons />
+    <appComments />
     <appFooter />
+    <appContacts />
   </div>
 </template>
 
@@ -32,7 +17,8 @@ import appAbout from "@/components/about.vue";
 import appOrders from "@/components/orders.vue";
 import appLessons from "@/components/lessons.vue";
 import appFooter from "@/components/footer.vue";
-import appComments from '@/components/comments.vue'
+import appComments from "@/components/comments.vue";
+import appContacts from "@/components/contacts.vue";
 
 export default {
   name: "Home",
@@ -42,6 +28,7 @@ export default {
     appOrders,
     appLessons,
     appComments,
+    appContacts,
     appFooter,
   },
 };
