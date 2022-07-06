b3a6dee7b242255d24863a6a740e5e85d875c4c4
Ivan Shavliuga (Ivanov) <38425718+IvanShavliuga@users.noreply.github.com>
2022-07-06 18:20:54 +0300

    created lessons component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,51 +3,7 @@
     <appHeader />
     <appAbout />
     <appOrders/>
-    <section class="lessons">
-      <div class="wrapper">
-        <h2>Как проходят занятия</h2>
-        <div class="item">
-          <div class="item__info">
-            <h3 class="item__info__header">Дыхательные техники</h3>
-            <p class="item__info__desc">
-              Специальные дыхательные упраэнения помогут сконцентрировать
-              внимание и сфокусировать сохнание на мышцах тела
-            </p>
-          </div>
-          <div class="item__image"><div class="item__image__photo1"></div></div>
-        </div>
-        <div class="item">
-          <div class="item__info">
-            <h3 class="item__info__header">Разминка</h3>
-            <p class="item__info__desc">
-              Перед началом занятий разогрев тела, прорабатываем мышцы шеи,
-              спины, выполняем упражнения на баланс под спокойную музыку
-            </p>
-          </div>
-          <div class="item__image"><div class="item__image__photo2"></div></div>
-        </div>
-        <div class="item">
-          <div class="item__info">
-            <h3 class="item__info__header">Основной блок</h3>
-            <p class="item__info__desc">
-              Упражнения выполняются на коврике без специального оборудования
-              или с малым оборудованиемв медленном темпе
-            </p>
-          </div>
-          <div class="item__image"><div class="item__image__photo3"></div></div>
-        </div>
-        <div class="item">
-          <div class="item__info">
-            <h3 class="item__info__header">Стрейчинг</h3>
-            <p class="item__info__desc">
-              В конце выполняем специальные упражнения для развития гибкости,
-              снятия усталости, стресса и мышечного напряжения
-            </p>
-          </div>
-          <div class="item__image"><div class="item__image__photo4"></div></div>
-        </div>
-      </div>
-    </section>
+    <appLessons/>
     <section class="comments">
       <div class="wrapper">
         <h2>Отзывы</h2>
@@ -118,7 +74,8 @@
 // @ is an alias to /src
 import appHeader from "@/components/header.vue";
 import appAbout from "@/components/about.vue";
-import appOrders from "@/components/order.vue";
+import appOrders from "@/components/orders.vue";
+import appLessons from "@/components/lessons.vue";
 import appFooter from "@/components/footer.vue";
 
 export default {
@@ -127,6 +84,7 @@ export default {
     appHeader,
     appAbout,
     appOrders,
+    appLessons,
     appFooter,
   },
 };
